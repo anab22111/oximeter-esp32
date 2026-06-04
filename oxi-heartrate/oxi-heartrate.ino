0b8ba3557483ddbaecf16bf7f3c6d452a10031ee
Doroteja Citakovic <dorotejacitakovic4@gmail.com>
2026-06-04 12:26:32 +0200

    .ino preuzet

--- oxi-heartrate/oxi-heartrate.ino
+++ oxi-heartrate/oxi-heartrate.ino
@@ -1,226 +1,208 @@
-/*
-  Optical Heart Rate Detection (PBA Algorithm) using the MAX30105 Breakout
-  By: Nathan Seidle @ SparkFun Electronics
-  Date: October 2nd, 2016
-  https://github.com/sparkfun/MAX30105_Breakout
-
-  This is a demo to show the reading of heart rate or beats per minute (BPM) using
-  a Penpheral Beat Amplitude (PBA) algorithm.
-
-  It is best to attach the sensor to your finger using a rubber band or other tightening
-  device. Humans are generally bad at applying constant pressure to a thing. When you
-  press your finger against the sensor it varies enough to cause the blood in your
-  finger to flow differently which causes the sensor readings to go wonky.
-
-  Hardware Connections (Breakoutboard to Arduino):
-  -5V = 5V (3.3V is allowed)
-  -GND = GND
-  -SDA = A4 (or SDA)
-  -SCL = A5 (or SCL)
-  -INT = Not connected
-
-  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
-  but it will also run at 3.3V.
-*/
-
 #include <Wire.h>
 #include "MAX30105.h"
 #include <PubSubClient.h>
-#include "heartRate.h"
-
+#include "spo2_algorithm.h"
 #include <WiFi.h>
+
+// ================= MREZNA podesavanje =================
 const char* ssid = "FTN_wifi";
 const char* password = "ftn12345";
+const char* mqtt_server = "10.1.148.251"; 
 
-const char* mqtt_server = "10.0.2.15 ";    // koristimo javni mqtt broker
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-WiFiClient espClient;              // iz Wifi.h biblioteke, koristi se da otvori vezu i slaj esiorve podatke preko interneta
-PubSubClient client(espClient);    // iz Pubsubclient.h biblioteke, PubSubClient pravi klijenta koji na MQTT protokol, ali nema pristup wifi, tako da mu se prosledjuje WiFiCLient
 long lastMsg = 0;
-char msg[50];
-int value = 0;
-const int sendInterval = 2000;
+const int sendInterval = 2000; 
 
+// ================= SENZOR podesavanja =================
 MAX30105 particleSensor;
 
-const byte RATE_SIZE = 10; //Increase this for more averaging. 4 is good.
-byte rates[RATE_SIZE]; //Array of heart rates
-byte rateSpot = 0;
-long lastBeat = 0; //Time at which the last beat occurred
+#define BUFFER_SIZE 100
+uint32_t irBuffer[BUFFER_SIZE]; 
+uint32_t redBuffer[BUFFER_SIZE]; 
+int32_t bufferIndex = 0;
+bool bufferFull = false;
 
-float beatsPerMinute;
-int beatAvg;
+// Izlazne promenljive iz algoritma
+int32_t spo2 = 0; 
+int8_t validSPO2 = 0; 
+int32_t heartRate = 0; 
+int8_t validHeartRate = 0;
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-  Serial.println("Initializing...");
+  delay(1000);
+  Serial.println("Inicijalizacija sistema...");
 
-  setup_wifi();    // povezi se na wifi
+  setup_wifi();
 
-  client.setServer(mqtt_server, 1883);        // postavi servera za esp32 da bude mqtt server koji je gore definisan
-  client.setCallback(callback);               //  postavljanje funkcije koja ce se pozivati kad stigne poruka
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 
-  Wire.begin(0,1);
-  // Initialize sensor
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
-  {
-    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+  Wire.begin(0, 1);
+  
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) {
+    Serial.println("MAX30105 nije pronađen!");
     while (1);
   }
-  Serial.println("Place your index finger on the sensor with steady pressure.");
+  Serial.println("Sistem spreman. Postavite prst stabilno.");
 
-  // particleSensor.setup(); //Configure sensor with default settings
-  // particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  // particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
-  byte ledBrightness = 50;  // Umerena jacina svetla (ne zaslepljuje senzor)
-  byte sampleAverage = 4;   // Usrednjavanje 4 uzorka unutar samog senzora (smanjuje sum)
+  // podesavanja optimalna za sakupljanje SpO2 bafera
+  byte ledBrightness = 60;  
+  byte sampleAverage = 1;   // Smanjeno na 1 da dobijemo sirovije i brze uzorke za m_algorithm
   byte ledMode = 2;         // 2 = Red + IR
-  byte sampleRate = 100;    // 100 uzoraka u sekundi
+  byte sampleRate = 100;    
   int pulseWidth = 411;     
   int adcRange = 4096;      
 
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
 }
 
-void loop()
-{
-
+void loop() {
   if (!client.connected()) {
-    reconnect();                 // ukoliko klijent nije povezan, pozovi reconnect
+    reconnect();
   }
-  client.loop();      // pokreni klijenta da slusa poruke
+  client.loop();    
 
+  // citamo sirove vrednosti
   long irValue = particleSensor.getIR();
+  long redValue = particleSensor.getRed();
 
-  if(irValue < 50000){
-    // ako nema prsta ne punimo bafer sa vrednostima
-    beatsPerMinute = 0;
-    beatAvg = 0;
-  }else{
-   if (checkForBeat(irValue) == true)
-    {
-      //We sensed a beat!
-      long delta = millis() - lastBeat;
-      lastBeat = millis();
+  // blaga provera za prisustvo prsta (samo ako je skroz skinut resetujemo)
+  if (irValue < 20000) { 
+    spo2 = 0;
+    validSPO2 = 0;
+    heartRate = 0;
+    validHeartRate = 0;
+    bufferIndex = 0;
+    bufferFull = false;
+  } else {
+    
+    if (!bufferFull) {           // ako nije pun ne vrsi se merenje
+      // punjnje bafera sa 100 uzoraka
+      redBuffer[bufferIndex] = redValue;
+      irBuffer[bufferIndex] = irValue;
+      bufferIndex++;
 
-      //beatsPerMinute = 60 / (delta / 1000.0);
-      beatsPerMinute = 60000.0 / (float)delta;
-
-      if (beatsPerMinute < 255 && beatsPerMinute > 20)
-      {
-        rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-        rateSpot %= RATE_SIZE; //Wrap variable
-
-        //Take average of readings
-        beatAvg = 0;
-        for (byte x = 0 ; x < RATE_SIZE ; x++)
-          beatAvg += rates[x];
-        beatAvg /= RATE_SIZE;
+      if (bufferIndex >= BUFFER_SIZE) {
+        bufferFull = true;
+        // Prvi proračun nad prvih 100 uzoraka
+        maxim_heart_rate_and_oxygen_saturation(irBuffer, BUFFER_SIZE, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
       }
+    } 
+    else {
+      
+      // cekamo da senzor javi da ima nov uzorak na I2C
+      while (particleSensor.available() == false) {
+        particleSensor.check();
+      }
+
+      // pomeramo svih 75 starih uzoraka ka pocetku niza (brisemo 25 najstarijih)
+      for (byte i = 25; i < BUFFER_SIZE; i++) {
+        redBuffer[i - 25] = redBuffer[i];
+        irBuffer[i - 25] = irBuffer[i];
+      }
+
+      // sakupljane novih 25 uzoraka
+      for (byte i = 75; i < BUFFER_SIZE; i++) {
+        while (particleSensor.available() == false) {
+          particleSensor.check();
+        }
+        redBuffer[i] = particleSensor.getRed();
+        irBuffer[i] = particleSensor.getIR();
+        particleSensor.nextSample(); // pomeranje unutrasnjeg pointer senzora za sl krug
+      }
+
+      // odmah racunanje sa noivm podacima
+      maxim_heart_rate_and_oxygen_saturation(irBuffer, BUFFER_SIZE, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
     }
   }
 
-  // slanje poruke
+  // ================= MQTT SLANJE PODATAKA =================
   long now = millis();
-  if(now - lastMsg > sendInterval){
+  if (now - lastMsg > sendInterval) {
     lastMsg = now;
     
-    // priprema stringova za slanje
     char tempStringBPM[8];
-    char tempStringAvg[8];
+    char tempStringSPO2[8];
     char tempStringIR[12];
 
-    // pretvaranje brojeva u char array (tekst) posto pubsubclient salje samo karaktere 
-    dtostrf(beatsPerMinute, 1, 2, tempStringBPM);    // decimal to string
-    dtostrf(beatAvg, 1, 0, tempStringAvg);             // 1 je minimalna sirina celog niza karaktera, 2 je koliko decmala
-    ltoa(irValue, tempStringIR, 10);             // mora ascii psoto je long prevelik, 10 je osnovni dekadni sistem
+    // za pamcenje prethdih vrendsit
+    static int32_t lastValidBPM = 75;
 
-    if (irValue < 50000) {
-      // ako nema prsta, salje se poruka gde su vrednosti nula
+    // ako algoritam vrati validan prst upisi ga, inace posalji 0
+    if (validHeartRate == 1 && heartRate > 40 && heartRate < 180 && irValue > 20000) {
+      lastValidBPM = (lastValidBPM * 0.7) + (heartRate * 0.3);  // 70% stare 30% nove
+      ltoa(lastValidBPM, tempStringBPM, 10);
+    }else {
+      // Ako je prst tu ali trenutno računa, zadrži poslednji dobar puls umesto nule
+      if (irValue > 20000 && lastValidBPM > 0) {
+        ltoa(lastValidBPM, tempStringBPM, 10);
+      } else {
+        strcpy(tempStringBPM, "0");
+      }
+    }
+    // ako algoritam vrati validan SpO2
+    if (validSPO2 == 1 && spo2 > 70 && spo2 <= 100 && irValue > 20000) {
+      ltoa(spo2, tempStringSPO2, 10);
+    } else {
+      strcpy(tempStringSPO2, "0");
+    }
+    
+    ltoa(irValue, tempStringIR, 10);
+
+    if (irValue < 20000) {
       client.publish("ftn/oksimetar/bpm", "0");
-      client.publish("ftn/oksimetar/avg_bpm", "0");
+      client.publish("ftn/oksimetar/spo2", "0");
       client.publish("ftn/oksimetar/status", "No finger?");
       Serial.println("MQTT: Nema prsta.");
     } else {
-      // ako je prst tu, posalji sve vrednosti
       client.publish("ftn/oksimetar/bpm", tempStringBPM);
-      client.publish("ftn/oksimetar/avg_bpm", tempStringAvg);
+      client.publish("ftn/oksimetar/spo2", tempStringSPO2);
       client.publish("ftn/oksimetar/ir", tempStringIR);
-      client.publish("ftn/oksimetar/status", "Izmereno");
+      client.publish("ftn/oksimetar/status", bufferFull ? "Izmereno" : "Punjenje bafera...");
       
-      Serial.print("MQTT Poslato");
-      Serial.print("IR=");
-      Serial.print(irValue);
-      Serial.print(", BPM=");
-      Serial.print(beatsPerMinute);
-      Serial.print(", Avg BPM=");
-      Serial.print(beatAvg);
-
+      Serial.print("MQTT Poslato -> ");
+      Serial.print("BPM: "); Serial.print(tempStringBPM);
+      Serial.print(" | SpO2: "); Serial.print(tempStringSPO2);
+      Serial.print("% | Validnost (HR/SpO2): "); Serial.print(validHeartRate); Serial.print("/"); Serial.println(validSPO2);
     }
   }
 
-  Serial.println();
-
-  //particleSensor.nextSample(); 
+  particleSensor.check(); 
   delay(10);
 }
 
-void setup_wifi(){   // funkcija za pvoezivanje na wifi
+void setup_wifi(){
   delay(10);
-
   Serial.println();
   Serial.print("Povezujem se na ");
   Serial.println(ssid);
-
-  // stavi u mode klijenta
   WiFi.mode(WIFI_STA);
-
-  // probaj da se konektujes na ftn wifi
   WiFi.begin(ssid, password);
 
-  // dokle god se ne poveze nemoj nista raditi
-   while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
-  Serial.print("Povezan na WiFi. ");
+  Serial.println("\nWiFi uspesno povezan!");
 }
 
+void callback(char* topic, byte* message, unsigned int length) {}
 
-
-void callback(char* topic, byte* message, unsigned int length){      // za sad ne znamo sta bi esp mogao da prima od poruka
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
-
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
-}
-
-void reconnect(){               // povezivanje esp32 na mqtt broker
+void reconnect() {
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-
-    String clientId = "ESP32C6Client";
-     // Attempt to connect
-    if (client.connect("clientId")) {      // posto je server postavljen u setup-u, klijent se povezuje nna njega - mqtt broker //  povezuje se na server sa id ESP32C6Client
-      Serial.println("connected");
-      client.subscribe("esp32/output");     // ne mora jer jos ne znamo koje poruke ce s aprima
-    }else{
-      Serial.print("failed, rc=");
+    Serial.print("Pokusavam MQTT povezivanje...");
+    String clientId = "ESP32C6Client-" + String(random(0xffff), HEX);
+    
+    if (client.connect(clientId.c_str())) {
+      Serial.println("povezan na broker!");
+    } else {
+      Serial.print("greska, rc=");
       Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
+      Serial.println(" nova proba za 5 sekundi");
       delay(5000);
     }
   }
 }
-
-
-
