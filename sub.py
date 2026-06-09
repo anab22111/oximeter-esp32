7e629d2193bbec53469e053bdd6444229d1c9d4c
Ana B <budimirovicana4@gmail.com>
2026-06-09 14:00:54 +0200

    dodato cuvanje podataka za korisnike

--- sub.py
+++ sub.py
@@ -20,9 +20,27 @@ try:
 except Exception as e:
     print(f"[GREŠKA] Neuspešno učitavanje zvuka: {e}")
 
+
+trenutni_korinsik = input("Unesite ime korisnika: ").strip()
+ime_fajla = f"merenje_{trenutni_korinsik}.txt" # Napravi fajl za upisivanje vrednosti datog korisnika
+
+# Definisanje promenljivih koje ce se koristiti pri snimanju podataka korisnika
+TRAJANJE_SNIMANJA = 30 # koliko sekundi cemo snimati korisnicke podatke
+snimanje_zapoceto = False
+snimanje_zavrseno = False
+vreme_pocetka_snimanja = 0
+
+# Kreiranje lista gde ce se cuvati bpm i spo2 kako bi se posle izracunao prosek
+svi_validni_bpm = []
+svi_validni_spo2 = []
+detektovan_zastoj = False
+
 # Funkcija koja se aktivira čim stigne poruka sa tvoje pločice
 def on_message(client, userdata, message):
     topic = message.topic
+
+    global snimanje_zapoceto, snimanje_zavrseno, vreme_pocetka_snimanja
+    global svi_validni_bpm, svi_validni_spo2, detektovan_zastoj
 
     if topic == "ftn/oksimetar/binarno":
         binary_payload = message.payload
@@ -38,6 +56,11 @@ def on_message(client, userdata, message):
         # 1. Obrada i prikaz Pulsa (BPM)
         if ir < 20000:
             print("Trenutni puls: -- BPM (Nema prsta)")
+            // Ukoliko je snimanje zapoceto i detektovano je da nema prsta - simulacija da nema otkucaja
+            // Postavi flag da je detektovan zastoj
+            if snimanje_zapoceto == True: 
+                detektovan_zastoj = True
+
         elif validBPM == 1:
             print(f"Trenutni puls: {bpm} BPM")
         else:
@@ -46,8 +69,10 @@ def on_message(client, userdata, message):
         # 2. Obrada i prikaz Kiseonika (SpO2)
         if ir < 20000:
             print("Zasićenost kiseonikom (SpO2): -- % (Nema prsta)")
-        elif validSPO2 == 1:
+        elif validSPO2 == 1:    
             print(f"Zasićenost kiseonikom (SpO2): {spo2}% [PODACI VALIDNI]")
+            if spo2 < 94:
+                print("[UPOZORENJE] Kriticno nizak nivo kiseonika")
         else:
             print("Zasićenost kiseonikom (SpO2): -- % [Čeka se merenje - bafer se puni]")
 
@@ -64,8 +89,59 @@ def on_message(client, userdata, message):
             # Čim se prst vrati, stopiraj reprodukciju zvuka
             pygame.mixer.music.stop()
 
-        print("-" * 40)    
+          
 
+        # 4. Zapisivanje odredjenih vrednosti koje stizu da senzora
+        # Pokreni snimanje cim je detektovan prst
+        if ir >= 20000 and not snimanje_zapoceto and not snimanje_zavrseno:
+            snimanje_zapoceto = True 
+            vreme_pocetka_snimanja = time.time()
+            print(f"Prst je detektovan. Zapoceto je snimanje za korisnika: {trenutni_korinsik}")
+        
+
+        if snimanje_zapoceto and not snimanje_zavrseno:
+            trenutno_vreme = time.time()
+
+            # Izmeri koliko vremena je preostalo za snimanje
+            preostalo_vreme = TRAJANJE_SNIMANJA - (trenutno_vreme - vreme_pocetka_snimanja)
+
+            # Ukoliko jos ima vremena nastavi snimanje
+            if preostalo_vreme > 0:
+
+                #if validBPM == 1 and bpm > 0:
+                svi_validni_bpm.append(bpm)
+                if validSPO2 == 1 and spo2 > 0:
+                    svi_validni_spo2.append(spo2)
+
+                # Otvori korisnikov fajl i upisi vrednosti
+                with open(ime_fajla, "a") as file:
+                    file.write(f"{vreme_ms},{ir}\n")
+            else:
+                # U suprotonom zaustavi snimanje
+                snimanje_zavrseno = True
+                snimanje_zapoceto = False
+
+                konacni_bpm = int(sum(svi_validni_bpm)/len(svi_validni_bpm) if svi_validni_bpm else 0)
+                konacni_spo2 = int(sum(svi_validni_spo2)/len(svi_validni_spo2) if svi_validni_spo2 else 0)
+                print(f"\n Uspešno snimljen fiksni uzorak od {TRAJANJE_SNIMANJA_SEKUNDE}s u '{ime_fajla}'.")
+                
+                status = ""
+                # Ukoliko je detektovan zastoj prilikom snimanja
+                if detektovan_zastoj:
+                    if svi_validni_bpm[-1] == 0:
+                        # ukoliko je psolednji sacuvani bpm bio 0 onda znaci da je bpm 0
+                        with open(ime_fajla, "a") as file:
+                            file.write(f"\nStatus: KRITICNO\nKonacan_BPM:0\nKonacan_SpO2:0\n")  
+                else:
+                    
+                    if konacni_spo2 < 94 and konacni_spo2 > 0:
+                        status = "KRITICNO"
+                    else:
+                        status = "NORMALNO"
+
+                    with open(ime_fajla, "a") as file:
+                            file.write(f"\nStatus: {status}\nKonacan_BPM:{konacni_bpm}\nKonacan_SpO2:{konacni_spo2}\n") 
+         print("-" * 40) 
 
 # Funkcija koja potvrđuje da se laptop uspešno povezao na HiveMQ
 def on_connect(client, userdata, flags, rc):
