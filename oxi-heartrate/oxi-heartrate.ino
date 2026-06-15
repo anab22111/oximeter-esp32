46f6b21ccab4c26c797bc4ad9fff6b248e85c973
Ana B <budimirovicana4@gmail.com>
2026-06-15 10:57:18 +0200

    dodata podesavanja za lcd

--- oxi-heartrate/oxi-heartrate.ino
+++ oxi-heartrate/oxi-heartrate.ino
@@ -1,10 +1,18 @@
+
+// koriscenje LovyanGFX biblioteke za prikaz stvari na lcd ekranu
+// posto LovyanGFX nema definisanu nasu plocu mi moramo da napravimo klasu i definisemo sve potrebne stavke
+// https://github.com/lovyan03/LovyanGFX/blob/master/examples/HowToUse/2_user_setting/2_user_setting.ino - postavka koja se prati
+#include <LovyanGFX.hpp>
 #include <Wire.h>
 #include "MAX30105.h"
 #include <PubSubClient.h>
 #include "spo2_algorithm.h"
 #include <WiFi.h>
 
-// ================= MREZNA podesavanje =================
+#define DRAW_EVERY_N 1  // crtaj svaki uzorak
+static uint32_t lastDrawTime = 0;
+
+// ================= MREZNA podesavanja =================
 const char* ssid = "FTN_wifi";
 const char* password = "ftn12345";
 const char* mqtt_server = "10.1.146.123"; 
@@ -24,58 +32,179 @@ struct __attribute__((packed)) Oksimetar {
   int8_t validSPO2;  // 1 bajt  (validSPO2: 0 ili 1)
 };
 
-MAX30105 particleSensor;
+MAX30105 particleSensor;             // inicijalizacija senzora
 
+// ================= DEFINISANJE PROMENLJIVIH ZA ALGORITAM =================
 #define BUFFER_SIZE 100
 uint32_t irBuffer[BUFFER_SIZE]; 
 uint32_t redBuffer[BUFFER_SIZE]; 
 int32_t bufferIndex = 0;
 bool bufferFull = false;
 
-// Izlazne promenljive iz algoritma
+// izlazne promenljive iz algoritma
 int32_t spo2 = 0; 
 int8_t validSPO2 = 0; 
 int32_t heartRate = 0; 
 int8_t validHeartRate = 0;
 
+// ================= DEFINISANJE PROMENLJIVIH ZA GRAF =================
+#define GRAPH_X 0
+#define GRAPH_Y 40     // gornjih 40 piksela rezervisano za tekst
+#define GRAPH_W 320    // sirina ekrana 
+#define GRAPH_H 132    // ostatak visine ekrana za graf (ukupno 172)
+#define GRAPH_MID (GRAPH_Y + (GRAPH_H / 2))
+static int16_t y_staro = GRAPH_MID; 
+
+// za potrebe crtanja grafa na displeju
+long sredisnjaVrednost = 250000; 
+static int32_t prikazaniBPM = 0;
+static int32_t prikazaniSPO2 = 0;
+
+static int16_t grafPozicija = 0;  // trenutna X pozicija crtanja
+static long minIR = 50000;
+static long maxIR = 100000;
+
+// ================= PRAVLJENJE KLASE ZA  ESP32C6 - LCD =================
+class LGFX_ESP32C6 : public lgfx::LGFX_Device  // klasa za nas uredjaj LGFX_ESP32C6
+{
+  lgfx::Panel_ST7789 _panel_instance;       // definisanje drajvera za ekran
+  lgfx::Bus_SPI _bus_instance;              // posto je ekran povezan sa kontrolerom preko spi, potrebno je i to definisati
+  lgfx::Light_PWM    _light_instance;       
+
+  public:
+    LGFX_ESP32C6(){   // konstruktor i konfiguracija/namestanje raznih podesavanja
+
+      auto bus_cfg = _bus_instance.config();
+
+      bus_cfg.spi_host = SPI2_HOST;      // SPI1 je rezervisan interno za fles memoriju cipa, pa se koristi SPI2
+
+      bus_cfg.spi_mode = 0;              // rezim rada - tacan trenutak takta - rastuca ivica takta
+
+      bus_cfg.freq_write = 27000000;     // frekvencija slanja podataka na ekran
+
+      bus_cfg.freq_read = 16000000;     // frekvencija citanja podataka sa ekrana - nama nije potrebno svejedno sta je
+
+      bus_cfg.spi_3wire = true;      // koriscenje razima bez MISO pina posto nama nije potreban, koristimo samo SCLK, CS, MOSI
+
+      bus_cfg.use_lock = true;          // zakljucavanje transakcija, kako jedan task ne bi prekinuo drugi
+
+      bus_cfg.dma_channel = SPI_DMA_CH_AUTO;    // postavlja DMA kanal koji ce koristiti SPI_DMA_CH_AUTO = automatsko podesavanje, DMA - Direct Memory Access, CPU odmara, DMA salje sliku na ekran u pozadini
+
+      // mapiranje pinova, gpio
+      bus_cfg.pin_sclk = 7;
+      bus_cfg.pin_mosi = 6;
+      bus_cfg.pin_miso = -1;             // posto ne koristimo -1
+      bus_cfg.pin_dc = 15;               // data/command, kako bi lcd znao da li mikrokontroler salje podatak(1) ili komandu(0)
+
+      _bus_instance.config(bus_cfg);    
+      _panel_instance.setBus(&_bus_instance);
+
+      //**************************//
+      // PODESAVANJE LCD EKRANA //
+      auto panel_cfg = _panel_instance.config();
+
+      panel_cfg.pin_cs = 14;                  // podesavanje chip select-a, kada esp32 spusti na nulu ovaj pin -> komunikacija sa ekranom
+      panel_cfg.pin_rst = 21;                 // reset
+      panel_cfg.pin_busy = -1;
+
+      panel_cfg.panel_width = 172; 
+      panel_cfg.panel_height = 320;
+
+      panel_cfg.offset_x = 34;  // <--- Pomera sliku u centar memorije drajvera
+      panel_cfg.offset_y = 0;
+
+      panel_cfg.offset_rotation  = 0;
+
+      panel_cfg.dummy_read_pixel = 8;     // Ostavi 8 (standardno za ST7789 drajver čip)
+      panel_cfg.dummy_read_bits  = 1;     // Ostavi 1
+
+      panel_cfg.readable = false;        // nemamo miso zicu tako da ne treba nista
+
+      panel_cfg.invert =  true;          // da pozadina postane crna, a ne plava/bela
+      panel_cfg.rgb_order = false;       // false jer se salju 8-bitni paketi
+      panel_cfg.bus_shared =  false;
+
+      _panel_instance.config(panel_cfg);
+
+      //********************************//
+      // PODESAVANJE POZADISNKOG SVETLA//
+      auto light_cfg = _light_instance.config();
+
+      light_cfg.pin_bl = 22;       // broj pina na koji je pozadinsko osvetljenje povezano
+      light_cfg.invert = false;
+      light_cfg.freq = 44100; 
+      light_cfg.pwm_channel = 1; 
+
+      _light_instance.config(light_cfg);
+      _panel_instance.setLight(&_light_instance);
+       
+      setPanel(&_panel_instance);
+    }
+};
+
+// kreiranje instance pripremljene klase
+LGFX_ESP32C6 display;
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
   Serial.println("Inicijalizacija sistema...");
+ 
+  Wire.begin(1, 0);
+  Wire.setClock(400000);
 
-  setup_wifi();
-
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-
-  Wire.begin(0, 1);
-  
+  // PODESAVANJA ZA SENZOR //
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) {
     Serial.println("MAX30105 nije pronađen!");
     while (1);
   }
   Serial.println("Sistem spreman. Postavite prst stabilno.");
 
+  // setup_wifi();
+
+  // client.setServer(mqtt_server, 1883);
+  // client.setCallback(callback);
+
   // podesavanja optimalna za sakupljanje SpO2 bafera
   byte ledBrightness = 60;  
-  byte sampleAverage = 1;   // Smanjeno na 1 da dobijemo sirovije i brze uzorke za m_algorithm
+  byte sampleAverage = 1;   // na 1 da dobijemo sirovije i brze uzorke za m_algorithm
   byte ledMode = 2;         // 2 = Red + IR
   byte sampleRate = 100;    
   int pulseWidth = 411;     
   int adcRange = 4096;      
 
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
+
+  // PODESAVANJA ZA ESP DISPLEJ //
+  // inicijalizacija SPI magistrale i panela
+  display.init();
+
+  display.setRotation(1);          // okreni horizontalno
+  display.setBrightness(200);      // pozadinsko svetlo na umerenoj jacini
+
+  display.fillScreen(TFT_BLACK);   // oboji ekran u crno
+
+  display.setTextSize(2);
+  display.setTextColor(TFT_GREEN, TFT_BLACK);
+  display.drawString("ECG: 75 BPM", 10, 10);
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();    
+  // if (!client.connected()) {
+  //   reconnect();
+  // }
+  // client.loop();    
 
   // citamo sirove vrednosti
   long irValue = particleSensor.getIR();
   long redValue = particleSensor.getRed();
+
+  // KONTINUALNO CRTANJE GRAFIKA: Crta se jedna tačka u svakom prolazu kroz loop
+  uint32_t now_ms = millis();
+  if (now_ms - lastDrawTime >= 10) {
+    lastDrawTime = now_ms;
+    crtajTackuNaEkranu(irValue);
+  }
 
   // slanje ir vrednosti za plotovanje
   if (irValue >= 20000) {
@@ -92,6 +221,10 @@ void loop() {
     validHeartRate = 0;
     bufferIndex = 0;
     bufferFull = false;
+    prikazaniBPM = 0;
+    prikazaniSPO2 = 0;
+    //*************************/
+    osveziTekstNaEkranu();
   } else {
     
     if (!bufferFull) {           // ako nije pun ne vrsi se merenje
@@ -102,46 +235,46 @@ void loop() {
 
       if (bufferIndex >= BUFFER_SIZE) {
         bufferFull = true;
-        // Prvi proračun nad prvih 100 uzoraka
+        // prvi proracun nad prvih 100 uzoraka
         maxim_heart_rate_and_oxygen_saturation(irBuffer, BUFFER_SIZE, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
       }
     } 
     else {
-      
-      // cekamo da senzor javi da ima nov uzorak na I2C
-      while (particleSensor.available() == false) {
-        particleSensor.check();
+      // Pomeri prvih 75 uzoraka na pocetak
+      for (byte i = 0; i < 75; i++) {
+        redBuffer[i] = redBuffer[i + 25];
+        irBuffer[i] = irBuffer[i + 25];
       }
 
-      // pomeramo svih 75 starih uzoraka ka pocetku niza (brisemo 25 najstarijih)
-      for (byte i = 25; i < BUFFER_SIZE; i++) {
-        redBuffer[i - 25] = redBuffer[i];
-        irBuffer[i - 25] = irBuffer[i];
-      }
-
-      // sakupljane novih 25 uzoraka
+      // Prikupi novih 25 uzoraka
       for (byte i = 75; i < BUFFER_SIZE; i++) {
         while (particleSensor.available() == false) {
           particleSensor.check();
         }
+        
         redBuffer[i] = particleSensor.getRed();
         irBuffer[i] = particleSensor.getIR();
-        particleSensor.nextSample(); // pomeranje unutrasnjeg pointer senzora za sl krug
-      }
-
-      // slanje i tokom punjenja bafera
-      if (irBuffer[i] >= 20000) {
+        
+        // Crtanje tokom punjenja — ne blokira grafik
+        crtajTackuNaEkranu(irBuffer[i]);
+        
+        // MQTT slanje tokom punjenja
+        if (irBuffer[i] >= 20000) {
           char irSubString[12];
           ltoa(irBuffer[i], irSubString, 10);
-          client.publish("ftn/oksimetar/sirovo", irSubString);
+          // client.publish("ftn/oksimetar/sirovo", irSubString);
         }
+        
+        particleSensor.nextSample();
+      }
 
-      // odmah racunanje sa noivm podacima
-      maxim_heart_rate_and_oxygen_saturation(irBuffer, BUFFER_SIZE, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+      // Racunanje nakon sto su 25 novih uzoraka prikupljeni
+      maxim_heart_rate_and_oxygen_saturation(irBuffer, BUFFER_SIZE, redBuffer,
+                                            &spo2, &validSPO2, &heartRate, &validHeartRate);
     }
   }
 
-  // ================= MQTT SLANJE PODATAKA (BINARNO) =================
+  // ================= MQTT SLANJE PODATAKA (BINARNO) I OSVEŽAVANJE EKRANA =================
   long now = millis();
   if (now - lastMsg > sendInterval) {
     lastMsg = now;
@@ -155,33 +288,37 @@ void loop() {
     // ako algoritam vrati validan prst upisi ga, inace posalji 0
     if (validHeartRate == 1 && heartRate > 40 && heartRate < 180 && irValue > 20000) {
       lastValidBPM = (lastValidBPM * 0.7) + (heartRate * 0.3);  // 70% stare 30% nove
-      //ltoa(lastValidBPM, tempStringBPM, 10);
       paket.bpm = lastValidBPM;
       paket.validBPM = 1;
-    }else {
-      // Ako je prst tu ali trenutno računa, zadrži poslednji dobar puls umesto nule
+      prikazaniBPM = lastValidBPM; // prosledjujemo istu validnu i filtriranu vrednost ekranu
+    } else {
+      // ako je prst tu ali trenutno racuna, zadrži poslednji dobar puls umesto nule
       if (irValue > 20000 && lastValidBPM > 0) {
-        //ltoa(lastValidBPM, tempStringBPM, 10);
         paket.bpm = lastValidBPM;
         paket.validBPM = 0;
+        prikazaniBPM = lastValidBPM;
       } else {
-        //strcpy(tempStringBPM, "0");
         paket.bpm = 0;
         paket.validBPM = 0;
+        prikazaniBPM = 0;
       }
     }
     // ako algoritam vrati validan SpO2
     if (validSPO2 == 1 && spo2 > 70 && spo2 <= 100 && irValue > 20000) {
-      //ltoa(spo2, tempStringSPO2, 10);
       paket.spo2 = spo2;
       paket.validSPO2 = 1;
+      prikazaniSPO2 = spo2; // prosledjujemo stabilan SpO2 ekranu
     } else {
-      //strcpy(tempStringSPO2, "0");
       paket.spo2 = 0;
       paket.validSPO2 = 0;
+      if (irValue < 20000) {
+        prikazaniSPO2 = 0;
+      }
     }
     
-    //ltoa(irValue, tempStringIR, 10);
+    // osvezavamo tekst na ekranu svake 2 sekunde u sinhronizaciji sa MQTT-om
+    osveziTekstNaEkranu();
+
     paket.ir = irValue;
 
     if (irValue < 20000) {
@@ -190,10 +327,10 @@ void loop() {
       paket.validBPM = 0;
       paket.validSPO2 = 0;
 
-      client.publish("ftn/oksimetar/binarno", (uint8_t*)&paket, sizeof(paket));
+      //client.publish("ftn/oksimetar/binarno", (uint8_t*)&paket, sizeof(paket));
       Serial.println("MQTT: Nema prsta (Poslat prazan binarni paket).");
     } else {
-      client.publish("ftn/oksimetar/binarno", (uint8_t*)&paket, sizeof(paket));
+      //client.publish("ftn/oksimetar/binarno", (uint8_t*)&paket, sizeof(paket));
       
       Serial.print("MQTT Poslato -> ");
       Serial.print("BPM: "); Serial.print(paket.bpm);
@@ -240,4 +377,66 @@ void reconnect() {
       delay(5000);
     }
   }
+}
+
+// funkcija koja osvezava tekst na vrhu ekrana
+void osveziTekstNaEkranu() {
+  display.setTextSize(2);
+  display.setCursor(10, 10);
+  
+  if (prikazaniBPM > 0 && prikazaniSPO2 > 0) {
+    display.setTextColor(TFT_GREEN, TFT_BLACK);
+    display.printf("HR: %d BPM  SpO2: %d%%              ", prikazaniBPM, prikazaniSPO2);
+  } else {
+    display.setTextColor(TFT_YELLOW, TFT_BLACK);
+    display.printf("Racunam... Postavite prst      ");
+  }
+}
+
+// funkcija za crtanje pulsnog signala
+void crtajTackuNaEkranu(long irValue) {
+  if (irValue < 20000) {
+    display.fillRect(grafPozicija, GRAPH_Y, 8, GRAPH_H, TFT_BLACK);
+    display.drawLine(grafPozicija == 0 ? 0 : grafPozicija - 1, 
+                     GRAPH_MID, grafPozicija, GRAPH_MID, TFT_RED);
+    grafPozicija++;
+    if (grafPozicija >= GRAPH_W) grafPozicija = 0;
+    return;
+  }
+
+  // FIKSNI prozor za srednju vrednost
+  // rolling average za sredinu, a FIKSNI opseg
+  sredisnjaVrednost = (sredisnjaVrednost * 0.98) + (irValue * 0.02);
+  
+  // fiksni opseg +-2000 oko srednje vrednosti (podesavanje po potrebi)
+  long vizMin = sredisnjaVrednost - 2000;
+  long vizMax = sredisnjaVrednost + 2000;
+
+  // map(trenutna_vrednost, donja_granica_trenutnog_signala, gornja_granica_trenutnog_signala, izlazni_min, izlazni_max)
+  // funkcija map uzima nasu ir vrednost koja se nalazi u prozoru (vizMin, vizMax) odredjuje njen procenat unutar prozora
+  // tj. koliko blizu/daleko vrha ili dna
+  // izlazni_min i izlazni_max u kom opsegu piksela zapravo crta
+  int16_t y_trenutno = map(irValue, vizMin, vizMax, 
+                           (GRAPH_Y + GRAPH_H - 5), (GRAPH_Y + 5));
+  y_trenutno = constrain(y_trenutno, GRAPH_Y + 2, GRAPH_Y + GRAPH_H - 2);
+
+  display.fillRect(grafPozicija + 1, GRAPH_Y, 8, GRAPH_H, TFT_BLACK);
+
+  if (grafPozicija > 0) {
+    // osnovna linija izmedju tacaka
+    display.drawLine(grafPozicija - 1, y_staro, 
+                     grafPozicija, y_trenutno, TFT_GREEN);
+    
+    // vertikalno popuni sve piksele izmedju y_staro i y_trenutno
+    // na trenutnoj X poziciji
+    int16_t yMin = min(y_staro, y_trenutno);
+    int16_t yMax = max(y_staro, y_trenutno);
+    for (int16_t y = yMin; y <= yMax; y++) {
+      display.drawPixel(grafPozicija, y, TFT_GREEN);
+    }
+  }
+
+  y_staro = y_trenutno;
+  grafPozicija++;
+  if (grafPozicija >= GRAPH_W) grafPozicija = 0;
 }
