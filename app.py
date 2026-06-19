a7b6ceb886a9840973c23c174bd330eab4d935bb
smiljakrulj <smilja.krulj04@gmail.com>
2026-06-20 00:50:02 +0200

    Komentari dodati

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
+"""
+Ovo je uproscena, test verzija web servera bez MQTT logike.
+Kompletna verzija se nalazi u sub.py, koja pored web servera 
+pokrece i MQTT klijenta, alarm i logiku snimanja u fajl.
+Ovaj fajl je koristan za testiranje web interfejsa odvojeno od hardvera.
+"""
+
 app = Flask(__name__)
-#Omogućavamo SocketIO i dopuštamo konekcije sa bilo koje adrese
+
+# Omogucavamo SocketIO i dopustamo konekcije sa bilo koje adrese
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-#1. Ruta koja otvara sajt
+# 1. Ruta koja otvara sajt
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#2. Slušalac za BPM podatke koji stižu iz sub.py
+# 2. Slusalac za BPM podatke koji stizu iz sub.py
+# Prima 'update_bpm' event i prosledjuje ga svim konektovanim browser-ima (broadcast=True)
 @socketio.on('update_bpm')
 def handle_bpm(data):
     emit('update_bpm', data, broadcast=True)
 
-#3. Slušalac za SpO2 podatke koji stižu iz sub.py
+# 3. Slusalac za SpO2 podatke koji stizu iz sub.py
+# Prima 'update_spo2' event i prosledjuje ga svim konektovanim browser-ima (broadcast=True)
 @socketio.on('update_spo2')
 def handle_spo2(data):
     emit('update_spo2', data, broadcast=True)
 
-#4. Slušalac za niz tačaka za PPG grafik koji stiže iz sub.py
+# 4. Slusalac za niz tacaka za PPG grafik koji stize iz sub.py
+# # Prima 'new_ppg_data' event sa nizom od 200 filtriranih IR vrednosti 
+# i prosledjuje ga svim konektovanim browser-ima (broadcast=True)
 @socketio.on('new_ppg_data')
 def handle_ppg(data):
     emit('new_ppg_data', data, broadcast=True)
